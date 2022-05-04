cea5501acbc88785ea3a6730deeb5e0f9deb32a0
Milan Gurung <milan.gurungnalim@gmail.com>
2022-05-04 11:36:52 +0545

    Change to package

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import pygeoip
+
+logINFO = open("logs.log", 'a+' )
+while True:
+	vic_ipad = input("\n\nEnter the Victim's IP address: ")
+	logINFO.write("\n%s\n" %(vic_ipad))
+	vic_ip = pygeoip.GeoIP("GeoLiteCity.dat")
+	llp = vic_ip.record_by_addr(vic_ipad)
+	for key, val in llp.items():
+		logINFO.write(str(key) + " :  " + str(val) + "\n")
+		print('%s : %s' % (key, val))
+logINFO.close()
